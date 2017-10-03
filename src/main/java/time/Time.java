69267c2b8a2e99d1663a3a285d5d7e605492c453
Tracy Tan <tracy.t@taboola.com>
2017-10-03 15:23:32 -0700

    Made change

--- src/main/java/time/Time.java
+++ src/main/java/time/Time.java
@@ -17,6 +17,7 @@ public class Time {
     public String getTime() {
         // @TODO: FIX THIS TOMORROW MORNING, FIRST THING. DON'T FORGET. AGAIN.
         // ಠ_ಠ
-        return null;
+        //return null;
+	return this.time;
     }
 }
